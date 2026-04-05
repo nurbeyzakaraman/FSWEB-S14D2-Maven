15abe4c4641a013d227fa0a80252c662d56a7e55
nurbeyzakaraman <nurbbkaraman@gmail.com>
2026-04-05 15:45:13 +0300

    Testler başarıyla geçildi

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
 
+import org.example.model.*;
+import org.example.model.enums.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    Wall northWall = new Wall("North");
+    Wall southWall = new Wall("South");
+    Wall eastWall = new Wall("East");
+    Wall westWall = new Wall("West");
+
+    Ceiling ceiling = new Ceiling(3, PaintColor.WHITE);
+    Bed bed = new Bed("Modern", 2, 50, 1, 1);
+    Lamp lamp = new Lamp(LampType.NEON, true, 5);
+    Wardrobe wardrobe = new Wardrobe(100, 200, 50.5);
+    Carpet carpet = new Carpet(200, 300, PaintColor.GREEN);
+
+    Bedroom myBedroom = new Bedroom(
+            "Nurbeyza's Room",
+            northWall, southWall, eastWall, westWall,
+            ceiling, bed, lamp, wardrobe, carpet
+    );
 }
